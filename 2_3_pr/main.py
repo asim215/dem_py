573d3636c85f5e5eb2513b1d8b7e43e1e49d9a32
Anton Simonov <asimonov215@gmail.com>
2022-10-05 23:42:14 +0300

    Refactor
    + Random answer
    * Data RegEx patterns to use key: []

--- 2_3_pr/main.py
+++ 2_3_pr/main.py
@@ -1,7 +1,23 @@
 # Модификация программы о регулярных выражениях
 
 import re
+import random
+
 import data_reg as d
+
+
+# Выбор случайного ответа
+def get_rand(key):
+    return random.choice(d.text[key])
+
+
+# Проверка всех вариантов по ключу
+def check_all(key, text):
+    for val in d.patterns[key]:
+        res = re.fullmatch(val, text)
+        if res:
+            return res
+    return None
 
 
 # В магазине
@@ -9,22 +25,22 @@ def shop():
     while True:
         ans = input("Вы в магазине. Что Вы будете делать? ").lower()
         # Вернуться
-        if re.fullmatch(d.patterns["м_верн"], ans):
+        if check_all("м_верн", ans):
             print("Вы вышли из магазина")
             break
         # Осмотреться
-        elif re.fullmatch(d.patterns["м_осм"], ans):
+        elif check_all("м_осм", ans):
             print("Вы видите много людей. "
                   "Сегодня выходной, поэтому многие закупаются "
                   "- предположили Вы.")
         # Выбор
-        elif re.fullmatch(d.patterns["м_выбор"], ans):
+        elif check_all("м_выбор", ans):
             print("Вы видите хлеб, молоко и кофемашину. Мда, не густо ...")
         # Взять
-        elif re.fullmatch(d.patterns["м_взять"], ans):
-            matchs = re.search(d.patterns["м_взять_s"], ans)
+        elif check_all("м_взять", ans):
+            # matchs = re.search(d.patterns["м_взять_s"], ans)
+            matchs = check_all("м_взять_s", ans)
             if matchs:
-                print(matchs[0])
                 if re.fullmatch(".*хлеб.*", matchs[0]):
                     print("Взятие хлеба успешно")
                 elif re.fullmatch(".*молоко.*", matchs[0]):
@@ -36,28 +52,32 @@ def shop():
                 else:
                     print("Не могу ", matchs[0], ". Этого нет на полке")
             else:
-                # !? Недостижимо
+                # ! Недостижимо
                 print("Не понятно, что именно вы хотите взять")
         else:
-            print("Вы задумались ...")
+            print(get_rand("nothing"))
 
 
 # Основной цикл
 end = False
 while not end:
     ans = input("Вы на улице. Ваши действия? ").lower()
-    if (re.fullmatch(d.patterns["выход"], ans)):
+    if check_all("выход", ans):
         # Выход из программы
-        print(d.text["gameover"])
+        print(get_rand("gameover"))
         end = True
-    elif (re.fullmatch(d.patterns["прогулка"], ans)):
+    elif check_all("прогулка", ans):
         # На прогулке
         ans = input("Куда именно? (в парк, в супермаркет) ")
-        if (re.fullmatch(d.patterns["парк"], ans)):
+        if check_all("парк", ans):
             print("Вы в парке")
-        elif (re.fullmatch(d.patterns["супермаркет"], ans)):
+        elif check_all("супермаркет", ans):
             print("Вы в супермаркете")
         else:
             print("Вы не знаете куда идти")
-    elif (re.fullmatch(d.patterns["магазин"], ans)):
+    elif check_all("магазин", ans):
+        # В магазин
         shop()
+    else:
+        # Неясный ответ
+        print(get_rand("unclear"))
