fc96eb9837566d0f803f8902ba82a68736516f62
Anton Simonov <asimonov215@gmail.com>
2022-10-13 23:07:25 +0300

    Refactor. Telegram bot

--- 3_3/main2.py
+++ 3_3/main2.py
@@ -22,7 +22,8 @@ class StateMachine(StatesGroup):
     user_info = State()
 
 
-def str_history(hs) -> str:
+# Преобразование структуры history в строку str для пользователя
+def history2str(hs) -> str:
     rs = ""
     for item in hs:
         rs += "дата: " + item.get("date", "-")
@@ -31,33 +32,41 @@ def str_history(hs) -> str:
     return rs
 
 
+# Информация для отладкий в терминал
+def log(message):
+    logging.info(f"{message.from_user.username}: {message.text}")
+
+
+# Начало работы с ботом
 @dp.message_handler(commands=['start', 'help'], state=StateMachine.user_info)
 async def send_welcome(message: types.Message):
+    # Исходные кнопки
     markup = ReplyKeyboardMarkup(resize_keyboard=True).add(
         KeyboardButton("/history")
     )
     await StateMachine.user_info.set()
-    await message.reply("Hi!\nIt is a testing BOT. It is in development.",
-                        reply_markup=markup)
-    await message.answer(f"Your id is {message.from_user.id}")
-    logging.info(f"{message.from_user.username}: {message.text}")
+    await message.reply("Привет.\n Я бот для тестирования, нахожусь\
+        в разработке. (v0.0.1)", reply_markup=markup)
+    await message.answer(f"Ваш id: {message.from_user.id}")
+    log(message)
 
 
+# Печать истории сообщений с пользователем
 @dp.message_handler(commands=['history'], state=StateMachine.user_info)
 async def history(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         history = data['history']
-
-    # await message.answer(f"Ваша история:\n {' '.join(history)}")
-    # await message.answer(f"Ваша история:\n {history}")
-    # await message.answer(f"Ваша история:\n {[str() for item in history] history}")
-    await message.answer(f"Ваша история:\n{str_history(history)}")
-    logging.info(f"{message.from_user.username}: {message.text}")
+    await message.answer(f"Ваша история:\n{history2str(history)}")
+    log(message)
 
 
+# Эхо ввода и запись в историю
+# Обработчик для каждого сообщения пользователя
 @dp.message_handler(state=StateMachine.user_info)
 async def echo(message: types.Message, state: FSMContext):
-    await message.answer(message.text)
+    # Эхо
+    # await message.answer(message.text)
+    # Запись истории сообщений
     async with state.proxy() as data:
         if "history" not in data:
             data["history"] = []
@@ -69,7 +78,7 @@ async def echo(message: types.Message, state: FSMContext):
                 "text": message.text,
             }
         )
-    logging.info(f"{message.from_user.username}: {message.text}")
+    log(message)
 
 
 if __name__ == '__main__':
